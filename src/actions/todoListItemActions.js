60f73fdae527d52a6a9c4104a22a73561a2533c8
Sami Karjalainen <sami.karjalainen@geniem.com>
2016-09-20 15:27:53 +0300

    Add api functions to client, add api calls to todoListItemActions, commented out for not until rest api is done

--- src/actions/todoListItemActions.js
+++ src/actions/todoListItemActions.js
@@ -1,8 +1,45 @@
+import api from '../services/api';
+
+/*export function addTodoListItem(name) {
+  return (dispatch) =>
+    api.post('/list-item', name)
+      .then(savedItem =>
+        dispatch({
+          type: 'TODO_LIST_ITEM_ADD',
+          name,
+        })
+      )
+      .catch(error =>
+        dispatch({
+          type: 'ERROR',
+          error,
+        })
+      );
+}
+
+export function removeTodoListItem(id) {
+  return (dispatch) =>
+    api.del('/order/'+id)
+      .then(() =>
+        dispatch({
+          type: 'TODO_LIST_ITEM_REMOVE',
+          id,
+        })
+      )
+      .catch(error =>
+        dispatch({
+          type: 'ERROR',
+          error,
+        })
+      );
+}
+*/
+
 export function addTodoListItem(name) {
   return (dispatch) =>
     dispatch({
       type: 'TODO_LIST_ITEM_ADD',
-      name
+      name,
     });
 }
 
