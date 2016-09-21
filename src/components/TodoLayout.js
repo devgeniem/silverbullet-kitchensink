1f9d98091464ef00dc69a0b813ed7b7cc1dd6255
Jani Matkala <jani@geniem.com>
2016-09-21 10:31:37 +0300

    Todo: styles n stuff.

--- src/components/TodoLayout.js
+++ src/components/TodoLayout.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+import {Glyphicon} from 'react-bootstrap';
 import {Link} from 'react-router';
 
 export default ({children}) => (
   <div className="todo-wrapper">
 
     <header className="todo-header-bar">
-      <Grid>
-        <Row>
-          <Col xs={1}>
-            <Link to="/"><img src="../images/logo.svg"
-                              alt=""/></Link>
-          </Col>
-          <Col xs={11}
-               className="todo-header-nav-items">
-            <Link to="/profile">Profile</Link>
-          </Col>
-        </Row>
-      </Grid>
-    </header>
+      <div>
+        <Link to="/">
+          <img src="../images/logo.svg"
+               alt=""/></Link>
+      </div>
+      <div>
+        <Link to="/profile">
+          <Glyphicon className="todo-header-bar-user-icon"
+                     glyph="user"/></Link>
+      </div>
 
-    <div className="todo-main-content">{ children }</div>
+    </header>
+    <div className="todo-main-content"> {children}</div>
   </div>
 );
