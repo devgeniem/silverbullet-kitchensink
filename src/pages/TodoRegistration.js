cc6cc648eb7e5cb2e8ff909fa77f8a7b21715ad3
Jani Matkala <jani@geniem.com>
2016-09-22 11:23:23 +0300

    Todo: Fixed Login/registration dummy pages

--- src/pages/TodoRegistration.js
+++ src/pages/TodoRegistration.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import {Input, Button} from 'react-bootstrap';
+import {FormControl, FormGroup, Button, Form, Grid, Row, Col} from 'react-bootstrap';
 
 export default class TodoRegistration extends React.Component {
 
-  render() {
-    return (<div className="todo-registration-container">
-      <Input type="text" placeholder="Email"/>
-      <Input type="password" placeholder="Password"/>
-      <Button className="todo-button">Register</Button>
-    </div>);
 
+  render() {
+
+    return (<div className="todo-registration-container">
+      <Grid>
+
+        <h1>Sign up!</h1>
+
+        <div className="todo-registration-box">
+
+
+          <Form>
+            <FormGroup>
+              <FormControl type="email" placeholder="Email"/>
+              <FormControl type="password" placeholder="Password"/>
+            </FormGroup>
+            <Row>
+              <Col xs={12}>
+                <Button className="todo-button">Sign up!</Button>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      </Grid>
+    </div>);
   }
 }
