eda238ee33df4ad7cd58e0d2bdf951bc339254c2
Sami Karjalainen <sami.karjalainen@geniem.com>
2016-09-22 14:25:49 +0300

    Fix lint errors

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -6,25 +6,25 @@
  */
 
 export default {
-    /**
-     * @api {post} /session Save session to server
-     * @apiName Save session
-     * @apiGroup Session
-     *
-     * @apiParam {Object} state Session state
-     *
-     * @apiSuccess {Object} Session
-     *
-     * @apiDescription
-     * Save session state to server
-     */
-    session: function (req, res) {
-        var params = req.params.all();
-        if (!params.state) {
-            return res.badRequest({ key: 'required_fields_missing', text: 'Parameter state required' });
-        }
-        delete params.state.routing; // don't save router state
-        req.session.state = params.state;
-        return res.ok(params.state);
-    },
+  /**
+   * @api {post} /session Save session to server
+   * @apiName Save session
+   * @apiGroup Session
+   *
+   * @apiParam {Object} state Session state
+   *
+   * @apiSuccess {Object} Session
+   *
+   * @apiDescription
+   * Save session state to server
+   */
+  session: function (req, res) {
+    var params = req.params.all();
+    if (!params.state) {
+      return res.badRequest({ key: 'required_fields_missing', text: 'Parameter state required' });
+    }
+    delete params.state.routing; // don't save router state
+    req.session.state = params.state;
+    return res.ok(params.state);
+  },
 };
--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,174 +1,169 @@
+import mailer from '../services/mailer';
+
 export default {
 
-    /**
-     * @api {get} /users users
-     * @apiName users
-     * @apiGroup User
-     *
-     * @apiSuccess {Object} user list
-     *
-     * @apiDescription
-     * Get organization user list
-     */
-    find: function (req, res) {
-        var criterias = {};
-        User.find(criterias)
-            .then(res.ok)
-            .catch(res.serverError);
-    },
+  /**
+   * @api {get} /users users
+   * @apiName users
+   * @apiGroup User
+   *
+   * @apiSuccess {Object} user list
+   *
+   * @apiDescription
+   * Get organization user list
+   */
+  find: function (req, res) {
+    var criterias = {};
+    User.find(criterias)
+      .then(res.ok)
+      .catch(res.serverError);
+  },
 
-    /**
-     * @api {get} /user/:id profile
-     * @apiName profile
-     * @apiGroup User
-     *
-     * @apiParam {String} id User ID
-     *
-     * @apiSuccess {Object} User profile
-     *
-     * @apiDescription
-     * Get user profile
-     */
-    findOne: function (req, res) {
-        var params = req.params.all();
-        User.findOne({ id: params.id })
-            .then(res.ok)
-            .catch(res.serverError);
-    },
+  /**
+   * @api {get} /user/:id profile
+   * @apiName profile
+   * @apiGroup User
+   *
+   * @apiParam {String} id User ID
+   *
+   * @apiSuccess {Object} User profile
+   *
+   * @apiDescription
+   * Get user profile
+   */
+  findOne: function (req, res) {
+    var params = req.params.all();
+    User.findOne({ id: params.id })
+      .then(res.ok)
+      .catch(res.serverError);
+  },
 
-    /**
-     * @api {post} /user user
-     * @apiName create user
-     * @apiGroup User
-     *
-     * @apiParam {String} name Name
-     * @apiParam {String} email Email
-     * @apiParam {String} password Password
-     *
-     * @apiSuccess {Object} User
-     *
-     * @apiDescription
-     * Create new user
-     */
-    create: function (req, res) {
-        var params = req.params.all();
+  /**
+   * @api {post} /user user
+   * @apiName create user
+   * @apiGroup User
+   *
+   * @apiParam {String} name Name
+   * @apiParam {String} email Email
+   * @apiParam {String} password Password
+   *
+   * @apiSuccess {Object} User
+   *
+   * @apiDescription
+   * Create new user
+   */
+  create: function (req, res) {
+    var params = req.params.all();
 
-        var data = {role: 'user'};
-        if (params.name) data.name = params.name;
-        if (params.email) data.email = params.email;
-        if (params.password) data.password = params.password;
+    var data = { role: 'user' };
+    if (params.name) data.name = params.name;
+    if (params.email) data.email = params.email;
+    if (params.password) data.password = params.password;
 
-        User.create(data)
-            .then(createdUser => {
-                mailer.sendUserActivationMail(createdUser);
-                res.ok(createdUser);
-            })
-            .catch(err => {
-                if (/already\sexists/m.test(err)) {
-                    res.json(403, {
-                        key: "already_exists",
-                        text: "User with that email already exists"
-                    });
-                } else {
-                    res.serverError(err);
-                }
-            });
-    },
+    User.create(data)
+      .then((createdUser) => {
+        mailer.sendUserActivationMail(createdUser);
+        res.ok(createdUser);
+      })
+      .catch((err) => {
+        if (/already\sexists/m.test(err)) {
+          res.json(403, {
+            key: 'already_exists',
+            text: 'User with that email already exists',
+          });
+        } else {
+          res.serverError(err);
+        }
+      });
+  },
 
-    /**
-     * @api {put} /user/:id update user by id
-     * @apiName update user by id
-     * @apiGroup User
-     *
-     * @apiParam {String} id User ID
-     * @apiParam {String} role Role (admin/user)
-     * @apiParam {String} email Email
-     * @apiParam {String} name Name
-     *
-     * @apiSuccess {Object} Updated user
-     *
-     * @apiDescription
-     * Update user profile by id, requires admin/superadmin privileges
-     */
-    update: function (req, res) {
-        var params = req.params.all();
+  /**
+   * @api {put} /user/:id update user by id
+   * @apiName update user by id
+   * @apiGroup User
+   *
+   * @apiParam {String} id User ID
+   * @apiParam {String} role Role (admin/user)
+   * @apiParam {String} email Email
+   * @apiParam {String} name Name
+   *
+   * @apiSuccess {Object} Updated user
+   *
+   * @apiDescription
+   * Update user profile by id, requires admin/superadmin privileges
+   */
+  update: function (req, res) {
+    var params = req.params.all();
 
-        var data = {};
-        if (params.role)
-            data.role = params.role;
-        if (params.email)
-            data.email = params.email;
-        if (params.name)
-            data.name = params.name;
+    var data = {};
+    if (params.role) data.role = params.role;
+    if (params.email) data.email = params.email;
+    if (params.name) data.name = params.name;
 
-        User.update({ id: params.id }, data)
-            .then((updated) => {
-                res.ok(updated[0]);
-            })
-            .catch(res.serverError);
-    },
+    User.update({ id: params.id }, data)
+      .then((updated) => {
+        res.ok(updated[0]);
+      })
+      .catch(res.serverError);
+  },
 
-    /**
-     * @api {delete} /user/:id remove user
-     * @apiName remove user
-     * @apiGroup User
-     *
-     * @apiParam {String} id User ID
-     *
-     * @apiSuccess {Boolean} Remove status
-     *
-     * @apiDescription
-     * Remove user, requires admin/superadmin privileges
-     */
-    destroy: function (req, res) {
-        var params = req.params.all();
-        User.destroy({ id: params.id })
-            .then(() => {
-                res.ok(true);
-            })
-            .catch(res.serverError);
-    },
+  /**
+   * @api {delete} /user/:id remove user
+   * @apiName remove user
+   * @apiGroup User
+   *
+   * @apiParam {String} id User ID
+   *
+   * @apiSuccess {Boolean} Remove status
+   *
+   * @apiDescription
+   * Remove user, requires admin/superadmin privileges
+   */
+  destroy: function (req, res) {
+    var params = req.params.all();
+    User.destroy({ id: params.id })
+      .then(() => {
+        res.ok(true);
+      })
+      .catch(res.serverError);
+  },
 
-    /**
-     * @api {get} /user/activate/:id activate
-     * @apiName activate
-     * @apiGroup User
-     *
-     * @apiParam {String} id User ID
-     * @apiParam {String} activationCode Activation code
-     *
-     * @apiSuccess {Boolean} Activation successful
-     *
-     * @apiDescription
-     * Activate user and save password
-     */
-    activate: function (req, res) {
+  /**
+   * @api {get} /user/activate/:id activate
+   * @apiName activate
+   * @apiGroup User
+   *
+   * @apiParam {String} id User ID
+   * @apiParam {String} activationCode Activation code
+   *
+   * @apiSuccess {Boolean} Activation successful
+   *
+   * @apiDescription
+   * Activate user and save password
+   */
+  activate: function (req, res) {
+    var params = req.params.all();
 
-        var params = req.params.all();
+    return User.findOne({ id: params.id })
+      .then((User) => {
+        if (User) {
+          if (!User.active && User.activationCode === params.activationCode) {
+            User.active = true;
+            return User.save()
+              .then(() => res.ok(true))
+              .catch(res.serverError);
+          }
+          return res.forbidden({
+            key: 'invalid_code_or_activation',
+            text: 'User is already activated or activation code is invalid',
+          });
+        }
+        return res.notFound({
+          key: 'not_found',
+          text: 'User not found',
+        });
+      })
+      .catch(res.serverError);
+  },
 
-        User.findOne({ id: params.id })
-            .then(User => {
-                if (User) {
-                    if (!User.active && User.activationCode === params.activationCode) {
-                        User.active = true;
-                        return User.save()
-                            .then(ok => {
-                                res.ok(true);
-                            })
-                            .catch(res.serverError);
-                    } else {
-                        res.forbidden({
-                            key: 'invalid_code_or_activation',
-                            text: 'User is already activated or activation code is invalid'
-                        });
-                    }
-                } else {
-                    res.notFound({
-                        key: 'not_found',
-                        text: 'User not found'
-                    });
-                }
-            })
-            .catch(res.serverError);
-    }
-}
+};
