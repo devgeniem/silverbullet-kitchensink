b1d10238f5e54c8a671c3526e230e7d3f7285e8a
Sami Karjalainen <sami.karjalainen@geniem.com>
2017-01-27 15:54:00 +0200

    Fix warnings

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,18 +3,16 @@ import { connect } from 'react-redux';
 import { Grid } from 'react-bootstrap';
 import LoginForm from '../forms/LoginForm';
 
-const Login = () => {
-  return (
-    <div className="todo-login-container">
-      <Grid>
-        <h1>Log in, mate!</h1>
-        <div className="todo-login-box">
-          <LoginForm />
-        </div>
-      </Grid>
-    </div>
-  );
-};
+const Login = () => (
+  <div className="todo-login-container">
+    <Grid>
+      <h1>Log in, mate!</h1>
+      <div className="todo-login-box">
+        <LoginForm />
+      </div>
+    </Grid>
+  </div>
+);
 
 function mapStateToProps(state) {
   return {
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -3,18 +3,16 @@ import { connect } from 'react-redux';
 import { Grid } from 'react-bootstrap';
 import RegisterForm from '../forms/RegisterForm';
 
-const Register = () => {
-  return (
-    <div className="todo-registration-container">
-      <Grid>
-        <h1>Sign up!</h1>
-        <div className="todo-registration-box">
-          <RegisterForm />
-        </div>
-      </Grid>
-    </div>
-  );
-};
+const Register = () => (
+  <div className="todo-registration-container">
+    <Grid>
+      <h1>Sign up!</h1>
+      <div className="todo-registration-box">
+        <RegisterForm />
+      </div>
+    </Grid>
+  </div>
+);
 
 function mapStateToProps(state) {
   return {
